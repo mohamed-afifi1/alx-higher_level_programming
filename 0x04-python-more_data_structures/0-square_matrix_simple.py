91b828742927a17261d656224e8c5c862c64fb78
root <root@DESKTOP-HU468P5.localdomain>
2024-02-08 19:31:50 +0200

    1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j] * matrix[i][j]
+            new_matrix[i][j] = new_matrix[i][j] * new_matrix[i][j]
     return new_matrix
