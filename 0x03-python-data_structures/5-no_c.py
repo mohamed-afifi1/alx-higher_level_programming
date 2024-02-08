3a013d6360e16e62e7d611dc1d275ec2c5a22c47
root <root@DESKTOP-HU468P5.localdomain>
2024-02-08 03:43:06 +0200

    from 0 to 6

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    length = len(my_string)
+
+    new = my_string[:]
+
+    for i in range(length):
+        if new[i] == 'c' or new[i] == 'C':
+            new = new[:i] + new[i+1:]
+    
+    return (new)
