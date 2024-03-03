28b55b1511c4fae3386a1ba6dfd45bd40419629f
root <root@DESKTOP-HU468P5.localdomain>
2024-03-03 22:02:06 +0200

    mandatory

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+
+This module is composed by a function that adds two numbers
+
+"""
+
+
+def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
+
+    Args:
+        a: first number
+        b: second number
+
+    Returns:
+        The addition of the two given numbers
+
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+
+    """
+
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
