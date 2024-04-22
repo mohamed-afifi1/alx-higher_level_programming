ac274034fcbfe223cf8877d7783d1e554a7dd139
root <root@DESKTOP-HU468P5.localdomain>
2024-04-22 18:53:13 +0200

    edit setter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -48,7 +48,7 @@ class Rectangle(Base):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
+        self.width = value
 
     @height.setter
     def height(self, value):
@@ -56,7 +56,7 @@ class Rectangle(Base):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = value
+        self.height = value
 
     @x.setter
     def x(self, value):
@@ -64,7 +64,7 @@ class Rectangle(Base):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
-        self.__x = value
+        self.x = value
 
     @y.setter
     def y(self, value):
@@ -72,7 +72,7 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
-        self.__y = value
+        self.y = value
 
     def area(self):
         """Return the area of the rectangle."""
