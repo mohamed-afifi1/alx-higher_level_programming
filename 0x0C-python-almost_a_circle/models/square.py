ba8edc1bdf3dbadb5e950e80fbbfe14ba6a16ac5
root <root@DESKTOP-HU468P5.localdomain>
2024-04-22 22:13:23 +0200

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,14 @@ from models.rectangle import Rectangle  # type: ignore
 class Square(Rectangle):
     """Represent a square."""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id=None)
+        """
+            Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
