7c08070caeb27d18c7eed5bd9a9e4fbd6b9afa4c
root <root@DESKTOP-HU468P5.localdomain>
2024-04-22 22:09:04 +0200

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This modules Defines a rectangle model class."""
+from models.rectangle import Rectangle  # type: ignore
+
+
+class Square(Rectangle):
+    """Represent a square."""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
