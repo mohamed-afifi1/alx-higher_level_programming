67691b91a1d0f7c18c201e07f4892964edaede36
root <root@DESKTOP-HU468P5.localdomain>
2024-06-24 22:55:42 +0300

    mandatory

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- list best scores
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC, name ASC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- update Bob
+UPDATE second_table
+set score = 10
+WHERE name = 'Bob';
