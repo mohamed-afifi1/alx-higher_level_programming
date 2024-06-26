a4f2d3d1484f55330bc24eb4f884ea64863729b5
root <root@DESKTOP-HU468P5.localdomain>
2024-06-26 22:57:18 +0300

    state model

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     from sqlalchemy.orm import Session
     from model_state import Base, State
 
-    search = sys.argv[4]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
