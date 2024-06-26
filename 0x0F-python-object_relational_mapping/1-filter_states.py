0267dacde76bd154492e6199d894394859d47bd2
root <root@DESKTOP-HU468P5.localdomain>
2024-06-26 19:17:11 +0300

    0:

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""  lists all N names states from the database hbtn_0e_0_usa """
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    mydb = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        database=database,
+        port=3306)
+    mycursor = mydb.cursor()
+    mycursor.execute("""select * from states where
+                     name like 'N%'
+                     order by states.id asc""")
+    for row in mycursor:
+        print(row)
+    mycursor.close()
+    mydb.close()
