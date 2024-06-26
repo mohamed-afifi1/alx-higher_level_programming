6e3d8c5c4cae64949addfeb6cd096a6a4f9b1225
root <root@DESKTOP-HU468P5.localdomain>
2024-06-26 20:50:26 +0300

    state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,6 @@ Base = declarative_base()
 class State(Base):
     """Represent a State."""
 
-    __tablename__ = 'state'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
