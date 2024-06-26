cef48b9aab0db7fd6ababac9e4bac16378f39521
root <root@DESKTOP-HU468P5.localdomain>
2024-06-26 19:30:04 +0300

    2:

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""  lists names states that validate from the database hbtn_0e_0_usa """
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search_name = sys.argv[4]
+    mydb = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        database=database,
+        port=3306)
+    mycursor = mydb.cursor()
+    mycursor.execute("""select * from states where
+                     name = %s
+                     order by
+                     states.id asc""",
+                     tuple(search_name))
+    for row in mycursor:
+        print(row)
+    mycursor.close()
+    mydb.close()
