2f90828c16e70b1788d68dcab42b6e6186c8a9c6
root <root@DESKTOP-HU468P5.localdomain>
2024-06-26 22:26:04 +0300

    state model

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     data = session.query(State).filter(State.name.like('%a%'))\
         .order_by(State.id).all()
     for state in data:
-        print("{}: {}".format(data.id, data.name))
+        print("{}: {}".format(state.id, state.name))
     session.close()
