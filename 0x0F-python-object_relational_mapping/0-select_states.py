4e8c2824be84a9bb15a205ee6eb7e633f4f37622
root <root@DESKTOP-HU468P5.localdomain>
2024-06-26 19:04:56 +0300

    0:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
 if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     mydb = MySQLdb.connect(
         host='localhost',
         user=username,
