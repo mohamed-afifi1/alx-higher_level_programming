9b6d756e14dba66da9f9618d3fec6dba0c67d85d
root <root@DESKTOP-HU468P5.localdomain>
2024-06-26 22:23:16 +0300

    state model

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""lists first State objects from the database hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    session = Session(engine)
+    data = session.query(State).filter(State.name.like('%a%'))\
+        .order_by(State.id).all()
+    for state in data:
+        print("{}: {}".format(data.id, data.name))
+    session.close()
