29b820447630ff077063fce1cbadc4344dbfa322
root <root@DESKTOP-HU468P5.localdomain>
2024-07-16 04:04:09 +0300

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,6 +5,8 @@ module.exports = class Square extends require('./5-square.js') {
   }
 
   charPrint (c) {
+    if (c === undefined)
+      c = 'X';
     for (let i = 0; i < this.width; i++) {
       console.log(c.repeat(this.width));
     }
